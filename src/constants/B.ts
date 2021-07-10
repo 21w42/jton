ef596350ee9368d0290fb2b496ec8d0aa63b6188
Kok Kek <kokkekpek@gmail.com>
2021-07-10 15:13:31 +0300

    Update constants

--- src/constants/B.ts
+++
@@ -1,2 +0,0 @@
-// Billion
-export const B: number = 1_000_000_000
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,5 @@
-export {B} from './B'
+export const Q: number = 1_000_000_000_000_000
+export const T: number = 1_000_000_000_000
+export const B: number = 1_000_000_000
+export const M: number = 1_000_000
+export const K: number = 1_000
