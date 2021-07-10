6cf3d56110759a273a1b4d9bad6c1aa91f6b2a2d
Kok Kek <kokkekpek@gmail.com>
2021-07-10 15:12:32 +0300

    Update delay function

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,9 @@
-export {delay} from './delay'
+/**
+ * Promise what resolve after the specified number of milliseconds.
+ * @param milliseconds
+ * Example:
+ *     1000
+ */
+export async function delay(milliseconds: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
