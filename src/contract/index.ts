d5efe9a7e5aeb188f0747622ced43be4386b346d
Kok Kek <kokkekpek@gmail.com>
2021-07-13 15:10:29 +0300

    Move GiverV2.se.keys.json from "contract" to "samples" directory.

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -1,8 +1,6 @@
 import transferAbi from '../contract/abi/transfer.abi.json'
-import GiverV2SeKeys from './keys/GiverV2.se.keys.json'
 
 export {transferAbi}
-export {GiverV2SeKeys}
 export {AccountType} from './enums/AccountType'
 export {ContractConfig} from './interfaces/ContractConfig'
 export {DeployedContractConfig} from './interfaces/DeployedContractConfig'
--- src/samples/index.ts
+++ src/samples/index.ts
@@ -1,6 +1,8 @@
+import GiverV2SeKeys from './GiverV2/GiverV2.se.keys.json'
 import GiverV2Contract from './GiverV2/GiverV2'
 import SafeMultisigWalletContract from './SafeMultisigWallet/SafeMultisigWallet'
 
+export {GiverV2SeKeys}
 export {GiverV2Contract}
 export {SafeMultisigWalletContract}
 export {GiverV2} from './GiverV2'
