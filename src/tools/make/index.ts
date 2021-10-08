ba0f67774145333d1f3656d733c8d51bd784abd2
Kok Kek <kokkekpek@gmail.com>
2021-10-09 01:29:03 +0300

    The make script config has been updated. Added `suffix`

--- src/tools/make/index.ts
+++ src/tools/make/index.ts
@@ -16,18 +16,19 @@ export interface MakeConfig {
     stdlib?: string
     extension?: Extension
     export?: Export
+    suffix?: string
 }
 
 export type Extension =
-    'ts' | // TypeScript
+    'ts' |  // TypeScript
     'tsx' | // TypeScript XML
-    'js' | // JavaScript
+    'js' |  // JavaScript
     'jsx'   // JavaScript XML
 
 export type Export =
     'commonjs' |
     'commonjs-default' | // CommonJS modules (NodeJS) with default export
-    'es6' | // ES6 modules
+    'es6' |              // ES6 modules
     'es6-default'        // ES6 modules with default export
 
 const errorConsoleTerminal: Terminal = new class implements Terminal {
@@ -44,6 +45,7 @@ const errorConsoleTerminal: Terminal = new class implements Terminal {
 
 const EXTENSION: Extension = 'ts'
 const EXPORT: Export = 'es6-default'
+const SUFFIX: string = 'Contract'
 const COMMAND: StringMap = {
     SOL_SET: 'sol set',
     SOL_COMPILE: 'sol compile',
@@ -78,6 +80,7 @@ export async function make(config: MakeConfig): Promise<void> {
     const printer: Printer = new Printer()
     const extension: Extension = config.extension ?? EXTENSION
     const exp: Export = config.export ?? EXPORT
+    const suffix: string = config.suffix ?? SUFFIX
     TonClient.useBinaryLibrary(libNode)
 
     await runCommand(errorConsoleTerminal, COMMAND.SOL_SET, {
@@ -90,14 +93,14 @@ export async function make(config: MakeConfig): Promise<void> {
     for (let i = 0; i < compileFiles.length; i++) {
         const file = compileFiles[i]
         await compile(config.root, file)
-        await wrap(config.root, exp, extension, file)
+        await wrap(config.root, exp, file, suffix, extension)
         printer.print(colors.green(file))
     }
 
     const wrapFiles: string[] = config.wrap ?? []
     for (let i = 0; i < wrapFiles.length; i++) {
         const file = wrapFiles[i]
-        await wrap(config.root, exp, extension, file)
+        await wrap(config.root, exp, file, suffix, extension)
         printer.print(colors.green(file))
     }
 }
@@ -124,16 +127,20 @@ async function compile(root: string, file: string): Promise<void> {
  * @param root Absolute root path.
  * Example:
  *     '/home/user/project/'
- * @param exp
- * @param extension
+ * @param exp 'commonjs', 'es6' ...
  * @param file Relative path without '.abi.json'.
  * Example:
  *     '/home/user/Project/nifi/contracts/Root'
+ * @param suffix Suffix to file name.
+ * If contract name is 'Root'
+ * and suffix is 'Contract'
+ * then result file name is 'RootContract'
+ * @param extension 'ts', 'js' ...
  */
-async function wrap(root: string, exp: Export, extension: Extension, file: string): Promise<void> {
+async function wrap(root: string, exp: Export, file: string, suffix: string, extension: Extension, ): Promise<void> {
     await runCommand(errorConsoleTerminal, COMMAND.JS_WRAP, {
         file: path.resolve(root, `${file}.${FILE.ABI_JSON}`),
         export: exp,
-        output: `${path.basename(file)}.${extension}`
+        output: `${path.basename(file)}${suffix}.${extension}`
     })
 }
